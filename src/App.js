0aeab36dc98b3ba75870e1de93cd9eb564ddac8f
RamonLazarotto <96747716+RamonLazarotto@users.noreply.github.com>
2025-06-03 17:31:51 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Menu from './componentes/Menu';
+import Menu from './componentes/Menu.js';
+import FormAutor from './paginas/FormAutor.js';
+import FormCategoria from './paginas/FormCategoria.js';
+import Home from './paginas/Home.js';
+import ListaAutor from './paginas/ListaAutor.js';
+import ListaCategoria from './paginas/ListaCategoria.js';
 
 function App() {
   return (
     <>
-      <Menu />
+      <BrowserRouter>
+        <Menu />
+
+        <div className='container'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/listacategoria' element={<ListaCategoria />} />
+            <Route path='/cadastrocategoria' element={<FormCategoria />} />
+            <Route path='/cadastrocategoria/:id' element={<FormCategoria />} />
+
+            <Route path='/listaautor' element={<ListaAutor />} />
+            <Route path='/cadastroautor' element={<FormAutor />} />
+            <Route path='/cadastroautor/:id' element={<FormAutor />} />
+
+            <Route path='*' element={<Home />} />
+          </Routes>
+        </div>
+
+
+
+
+      </BrowserRouter>
     </>
   );
 }
--- src/componentes/Menu.js
+++ src/componentes/Menu.js
@@ -1,41 +1,71 @@
-export default function Menu(){
-    return(
+export default function Menu() {
+    return (
         <>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-               <div className="container-fluid">
-                 <a className="navbar-brand" href="index.html">
-                   Navbar
-                 </a>
-                 <div className="collapse navbar-collapse">
-                   <ul className="navbar-nav me-auto">
-                     <li className="nav-item">
-                       <a className="nav-link active" href="index.html">
-                         Home
-                       </a>
-                     </li>
-                     <li className="nav-item">
-                       <a className="nav-link" href="listagem.html">
-                         Listagem
-                       </a>
-                     </li>
-                     <li className="nav-item">
-                       <a className="nav-link" href="cadastro.html">
-                         Cadastro
-                       </a>
-                     </li>
-                   </ul>
-                   <form className="d-flex">
-                     <input
-                       className="form-control me-2"
-                       type="search"
-                       placeholder="Search"
-                     />
-                     <button className="btn btn-outline-light" type="submit">
-                       Pesquisar
-                     </button>
-                   </form>
-                 </div>
-               </div>
+            <nav className="navbar navbar-expand-lg navbar-light bg-primary">
+                <div className="container-fluid">
+                    <a className="navbar-brand" href="#">
+                        Navbar
+                    </a>
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarSupportedContent"
+                        aria-controls="navbarSupportedContent"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
+                        <span className="navbar-toggler-icon" />
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <a className="nav-link active" aria-current="page" href="/">
+                                    Home
+                                </a>
+                            </li>
+                            <li className="nav-item">
+                                <a className="nav-link" href="#">
+                                    Link
+                                </a>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <a
+                                    className="nav-link dropdown-toggle"
+                                    href="#"
+                                    id="navbarDropdown"
+                                    role="button"
+                                    data-bs-toggle="dropdown"
+                                    aria-expanded="false"
+                                >
+                                    Listagens
+                                </a>
+                                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <li>
+                                        <a className="dropdown-item" href="/listacategoria">
+                                            Categorias
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a className="dropdown-item" href="/listaautor">
+                                            Autores
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <hr className="dropdown-divider" />
+                                    </li>
+                                    <li>
+                                        <a className="dropdown-item" href="#">
+                                            Something else here
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+
+                        </ul>
+
+                    </div>
+                </div>
             </nav>
 
         </>
